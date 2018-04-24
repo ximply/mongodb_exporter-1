4bb96fcdea43c537da329e4be1a99968eded58e1
ximply <void--@hotmail.com>
2018-04-24 16:04:45 +0800

    change mode to unix sock

--- mongodb_exporter.go
+++ mongodb_exporter.go
@@ -31,6 +31,7 @@ import (
 	"gopkg.in/yaml.v2"
 
 	"github.com/percona/mongodb_exporter/collector"
+	"net"
 )
 
 const (
@@ -46,7 +47,7 @@ func mongodbDefaultURI() string {
 
 var (
 	versionF       = flag.Bool("version", false, "Print version information and exit.")
-	listenAddressF = flag.String("web.listen-address", ":9216", "Address to listen on for web interface and telemetry.")
+	listenAddressF = flag.String("unix-sock", "/dev/shm/mongodb_exporter.sock", "Address to listen on for unix sock access and telemetry.")
 	metricsPathF   = flag.String("web.metrics-path", "/metrics", "Path under which to expose metrics.")
 	authFileF      = flag.String("web.auth-file", "", "Path to YAML file with server_user, server_password options for http basic auth (overrides HTTP_AUTH env var).")
 	sslCertFileF   = flag.String("web.ssl-cert-file", "", "Path to SSL certificate file.")
@@ -161,57 +162,21 @@ func startWebServer() {
 	collector := registerCollector()
 	defer collector.Close()
 
-	if (*sslCertFileF == "") != (*sslKeyFileF == "") {
-		log.Fatal("One of the flags -web.ssl-cert-file or -web.ssl-key-file is missing to enable HTTPS/TLS")
-	}
-	ssl := false
-	if *sslCertFileF != "" && *sslKeyFileF != "" {
-		if _, err := os.Stat(*sslCertFileF); os.IsNotExist(err) {
-			log.Fatal("SSL certificate file does not exist: ", *sslCertFileF)
-		}
-		if _, err := os.Stat(*sslKeyFileF); os.IsNotExist(err) {
-			log.Fatal("SSL key file does not exist: ", *sslKeyFileF)
-		}
-		ssl = true
-		log.Infoln("HTTPS/TLS is enabled")
-	}
+	os.Remove(*listenAddressF)
 
-	if ssl {
-		// https
-		mux := http.NewServeMux()
-		mux.Handle(*metricsPathF, handler)
-		mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-			w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
-			w.Write(landingPage)
-		})
-		tlsCfg := &tls.Config{
-			MinVersion:               tls.VersionTLS12,
-			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-			PreferServerCipherSuites: true,
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-			},
-		}
-		srv := &http.Server{
-			Addr:         *listenAddressF,
-			Handler:      mux,
-			TLSConfig:    tlsCfg,
-			TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
-		}
-		log.Infof("Starting HTTPS server on https://%s%s ...", *listenAddressF, *metricsPathF)
-		log.Fatal(srv.ListenAndServeTLS(*sslCertFileF, *sslKeyFileF))
-	} else {
-		// http
-		http.Handle(*metricsPathF, handler)
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			w.Write(landingPage)
-		})
-		log.Infof("Starting HTTP server on http://%s%s ...", *listenAddressF, *metricsPathF)
-		log.Fatal(http.ListenAndServe(*listenAddressF, nil))
+	mux := http.NewServeMux()
+	mux.Handle(*metricsPathF, handler)
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write(landingPage)
+	})
+	server := http.Server{
+		Handler: mux, // http.DefaultServeMux,
 	}
+	listener, err := net.Listen("unix", *listenAddressF)
+	if err != nil {
+		panic(err)
+	}
+	server.Serve(listener)
 }
 
 func registerCollector() *collector.MongodbCollector {
